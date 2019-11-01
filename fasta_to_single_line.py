19b769e030c06eb6fe427faa41538e4eef36c003
symPiotr <p.lukasik@gmail.com>
2019-11-01 23:49:38 +0100

    add fasta_to_single_line.py, a simple script that does exactly what the name says

---
+++ fasta_to_single_line.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+	sys.exit('This script reverse-complements all sequences within a multifasta file.\n'
+	         'Usage: ./fasta_to_single_line.py <fasta_file> \n')
+Script, Fasta = sys.argv
+
+FASTA = open(Fasta, 'r')
+
+
+Sequence_list = []
+Sequence = ''
+for line in FASTA:   # Copying the sequence (potentially spread across multiple lines) to a single line
+    if line.startswith('>'):
+       if Sequence != '':   # Saves the existing Seq_heading and Sequence to a list before overwriting them
+          Sequence_list.append([Seq_heading, Sequence])
+          Sequence = ''
+       Seq_heading = line.strip('\n')
+    else:
+       Sequence = Sequence + line.strip('\n').upper()
+
+Sequence_list.append([Seq_heading, Sequence]) # Saves the final sequence (Seq_heading and Sequence) to a list
+       
+FASTA.close()
+
+for entry in Sequence_list:
+   print(entry[0], '\n', entry[1], '\n', end = '', sep = '', file = sys.stdout)
